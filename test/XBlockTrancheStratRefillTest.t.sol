fe24def0399967163e5b38ec13242aedff6076a1
Siddharth2207 <siddharthbhoite22@gmail.com>
2024-02-03 21:04:02 +0530

    tests

---
+++ test/XBlockTrancheStratRefillTest.t.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: CAL
+pragma solidity =0.8.19;
+
+import {Vm} from "forge-std/Vm.sol";
+import {console2, Test} from "forge-std/Test.sol";
+import {
+    XBlockStratUtil,
+    IInterpreterV2,
+    IInterpreterStoreV1,
+    SourceIndexV2,
+    StateNamespace,
+    LibNamespace,
+    FullyQualifiedNamespace    
+} from "test/util/XBlockStratUtils.sol";
+
+import {
+    LOCK_TOKEN,
+    WETH_TOKEN,
+    LOCK_TOKEN_HOLDER,
+    WETH_TOKEN_HOLDER,
+    VAULT_ID,
+    OrderV2,
+    LOCK_OWNER,
+    SafeERC20,
+    IERC20,
+    IO
+} from "src/XBlockStratTrancheRefill.sol";
+
+interface IHoudiniSwapToken { function launch() external; }
+
+contract XBlockTrancheStratRefillTest is XBlockStratUtil { 
+
+    using SafeERC20 for IERC20;
+    address constant TEST_ORDER_OWNER = address(0x84723849238); 
+
+    function launchLockToken() public { 
+        vm.startPrank(LOCK_OWNER);
+        IHoudiniSwapToken(address(LOCK_TOKEN)).launch();
+        vm.stopPrank();
+    }
+
+    function lockIo() internal pure returns (IO memory) {
+        return IO(address(LOCK_TOKEN), 18, VAULT_ID);
+    }
+
+    function wethIo() internal pure returns (IO memory) {
+        return IO(address(WETH_TOKEN), 18, VAULT_ID);
+    }
+
+    function testTrancheRefillParser() public {
+
+        launchLockToken();
+        {
+            uint256 depositAmount = 1e18;
+            giveTestAccountsTokens(WETH_TOKEN, WETH_TOKEN_HOLDER, TEST_ORDER_OWNER, depositAmount);
+            depositTokens(TEST_ORDER_OWNER, WETH_TOKEN, VAULT_ID, depositAmount);
+        }
+        OrderV2 memory trancheOrder;  
+        {
+            (bytes memory bytecode, uint256[] memory constants) = PARSER.parse(getTrancheRefillOrder());
+            trancheOrder = placeOrder(TEST_ORDER_OWNER, bytecode, constants, lockIo(), wethIo());
+        }
+
+        takeOrder(trancheOrder, getEncodedLockBuyRoute());
+
+        
+    }
+    
+}
