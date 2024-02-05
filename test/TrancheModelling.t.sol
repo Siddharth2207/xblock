4edf39b352105cd71b4ebbacfc3f24e5492baa2a
thedavidmeister <thedavidmeister@gmail.com>
2024-02-05 22:17:40 +0400

    wip on fuzz binding

--- test/TrancheModelling.t.sol
+++ test/TrancheModelling.t.sol
@@ -10,7 +10,7 @@ import {
     SourceIndexV2,
     StateNamespace,
     LibNamespace,
-    FullyQualifiedNamespace    
+    FullyQualifiedNamespace
 } from "test/util/XBlockStratUtils.sol";
 import {LibEncodedDispatch} from "rain.orderbook/lib/rain.interpreter/src/lib/caller/LibEncodedDispatch.sol";
 import {
@@ -23,6 +23,20 @@ import {
 import "rain.orderbook/lib/rain.math.fixedpoint/src/lib/LibFixedPointDecimalArithmeticOpenZeppelin.sol";
 import "rain.orderbook/lib/rain.math.fixedpoint/src/lib/LibFixedPointDecimalScale.sol";
 import "rain.orderbook/lib/rain.interpreter/src/lib/bitwise/LibCtPop.sol";
+import {STACK_TRACER} from "rain.orderbook/lib/rain.interpreter/src/lib/state/LibInterpreterStateNP.sol";
+
+contract TracerLogger {
+    fallback() external {
+        bytes memory data;
+        assembly ("memory-safe") {
+            data := mload(0x40)
+            mstore(data, calldatasize())
+            calldatacopy(add(data, 0x20), 0, calldatasize())
+            mstore(0x40, add(data, add(calldatasize(), 0x20)))
+        }
+        console2.logBytes(data);
+    }
+}
 
 contract XBlockModelling is XBlockStratUtil {
     using LibFixedPointDecimalArithmeticOpenZeppelin for uint256;
@@ -30,6 +44,48 @@ contract XBlockModelling is XBlockStratUtil {
 
     uint256 lastUpdateTimeKey = uint256(keccak256(abi.encodePacked(ORDER_HASH, uint256(1))));
     uint256 trancheSpaceKey = uint256(keccak256(abi.encodePacked(ORDER_HASH, uint256(0))));
+
+    function testChartTrancheSpace(uint256 trancheSpace) external {
+        string[] memory ffi = new string[](19);
+        ffi[0] = "rain";
+        ffi[1] = "dotrain";
+        ffi[2] = "compose";
+        ffi[3] = "-i";
+        ffi[4] = "src/tranche-strat-refill.rain";
+        ffi[5] = "--entrypoints";
+        ffi[6] = "calculate-io-test";
+        ffi[7] = "--entrypoints";
+        ffi[8] = "calculate-io-test";
+        ffi[9] = "--bind";
+        ffi[10] = string.concat("test-tranche-space-before=", uint2str(trancheSpace));
+        ffi[11] = "--bind";
+        ffi[12] = "test-base-tranche-size=1000e18";
+        ffi[13] = "--bind";
+        ffi[14] = "test-now=0";
+        ffi[15] = "--bind";
+        ffi[16] = "test-last-update-time=0";
+        ffi[17] = "--bind";
+        ffi[18] = "test-io-ratio-multiplier=1e18";
+
+        bytes memory rainlang = vm.ffi(ffi);
+
+        address tracerLogger = address(new TracerLogger());
+        vm.etch(STACK_TRACER, tracerLogger.code);
+
+        uint256[][] memory context = new uint256[][](0);
+        FullyQualifiedNamespace namespace =
+            LibNamespace.qualifyNamespace(StateNamespace.wrap(uint256(uint160(ORDER_OWNER))), address(ORDERBOOK));
+        (bytes memory bytecode, uint256[] memory constants) = PARSER.parse(rainlang);
+        (,,address expression,) = EXPRESSION_DEPLOYER.deployExpression2(bytecode, constants);
+
+        (uint256[] memory stack, uint256[] memory kvs) = IInterpreterV2(INTERPRETER).eval2(
+            IInterpreterStoreV1(address(STORE)),
+            namespace,
+            LibEncodedDispatch.encode2(expression, SourceIndexV2.wrap(0), type(uint16).max),
+            context,
+            new uint256[](0)
+        );
+    }
 
     function test_trancheModelling() public {
         string memory file = './test/csvs/tranche-space-stack.csv';
@@ -47,7 +103,7 @@ contract XBlockModelling is XBlockStratUtil {
         ));
 
         for (uint256 i = 0; i < 200; i++) {
-        
+
             uint256 trancheSpace = uint256(1e17*i);
             vm.mockCall(address(STORE), abi.encodeWithSelector(IInterpreterStoreV1.get.selector, namespace, trancheSpaceKey), abi.encode(trancheSpace));
             vm.mockCall(address(STORE), abi.encodeWithSelector(IInterpreterStoreV1.get.selector, namespace, lastUpdateTimeKey), abi.encode(block.timestamp));
@@ -89,7 +145,7 @@ contract XBlockModelling is XBlockStratUtil {
             "Amount",
             ",",
             "Price"
-        
+
         ));
 
         for (uint256 i = 0; i < (48*8); i++) {
@@ -136,7 +192,7 @@ contract XBlockModelling is XBlockStratUtil {
         ));
 
         for (uint256 i = 0; i < 200; i++) {
-        
+
             uint256 trancheSpace = uint256(1e18*i);
             vm.mockCall(address(STORE), abi.encodeWithSelector(IInterpreterStoreV1.get.selector, namespace, trancheSpaceKey), abi.encode(trancheSpace));
             vm.mockCall(address(STORE), abi.encodeWithSelector(IInterpreterStoreV1.get.selector, namespace, lastUpdateTimeKey), abi.encode(block.timestamp));
